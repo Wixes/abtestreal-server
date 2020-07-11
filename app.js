331683568880f096c73480c0672b803b80b29f41
Wixes <dmitri.koma@yandex.ru>
2020-07-12 03:03:18 +0700

    trying to make heroku works

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.POR || 3000;
+
+app.use(cors());
+app.use('/api', routes);
+
+app.listen(port, () => {
+    console.log('server is listening');
+});
